6db276a6d4149753d419862b15a5ae134e3d0130
Mauricio Figueroa <mfigueroapa@hotmail.com>
2020-10-26 10:41:50 -0600

    falling projectile

--- index.js
+++ index.js
@@ -5,8 +5,11 @@ let frames = 0
 const gravity = 0.98
 const friction = 0.9
 const keys = []
+let meteorites = []
+let ratio = 200
 
 window.onload = () => {
+    // loadMenu()
     startGame()
 }
 
@@ -17,10 +20,9 @@ class Board {
         this.width = $canvas.width
         this.height = $canvas.height
         this.img = new Image()
-        this.img.src = ('./images/satellite.jpg')
+        this.img.src = ('./images/menu.jpg')
     }
     draw() {
-        // console.log("func draw from Board called")
         $context.drawImage(this.img, this.x, this.y, $canvas.width, $canvas.height)
     }
 }
@@ -32,7 +34,7 @@ class Alien {
         this.y = $canvas.height - this.height
         this.velX = 0
         this.img = new Image()
-        this.img.src = ('./images/alien1.png')
+        this.img.src = ('./images/char1-front.png')
     }
     draw() {
         // console.log("func draw from Alien called")
@@ -44,42 +46,85 @@ class Alien {
         this.x += this.velX
         this.velX *= friction
     }
-    // moveLeft() {
-    //     console.log("moveL")
-    //     this.x -= 15
-    // }
-    // moveRight() {
-    //     console.log("moveR")
-    //     this.x += 15
-    // }
-
 }
+class Obstacle {
+    constructor() {
+        this.y = 10
+        this.x = 350
+        this.height = 130
+        this.width = 50
+        this.img = new Image()
+        this.img.src = ('./images/meteorite.png')
+    }
+    draw() {
+        this.y += 4
+        $context.drawImage(this.img, this.x, this.y, this.width, this.height)
+    }
+}
+
+// class Menu {
+//     constructor() {
+//         this.x = 0
+//         this.y = 0
+//         this.width = $canvas.width
+//         this.height = $canvas.height
+//         this.img = new Image()
+//         this.img.src = ('./images/menu.jpg')
+//     }
+//     drawB() {
+//         $context
+//     }
+//     draw() {
+//         $context.drawImage(this.img, this.x, this.y, $canvas.width, $canvas.height)
+//     }
+// }
 
 function startGame() {
     console.log("game started")
     if (gameInterval) return
     gameInterval = setInterval(updateGame, 1000 / 60)
 }
-
+function generateMeteorites() {
+    if (frames % ratio === 0) {
+        meteorites.push(new Obstacle())
+        // console.log(obstacles)
+    }
+}
+function drawMeteorites() {
+    meteorites.forEach(obs => obs.draw())
+}
+function clearMeteorites() {
+    meteorites = meteorites.filter(obs => obs.y > -obs.height)
+}
+function loadMenu() {
+    mainMenu.draw()
+    console.log("main menu")
+}
 function clearCanvas() {
     $context.clearRect(0, 0, $canvas.width, $canvas.height)
 }
 
 function updateGame() {
-    // frames++
+    frames++
+
     checkKeys()
     alien.changePos()
+
+    clearMeteorites()
     clearCanvas()
+
     board.draw()
     alien.draw()
-    
-}
 
+    generateMeteorites()
+    drawMeteorites()
+}
+// let mainMenu = new Menu() //create main menu
 let board = new Board() //create board instance
 let alien = new Alien() //create alien instance 
 
-//Control
 
+//Control
 function checkKeys() {
     if (keys["ArrowLeft"]) {
         alien.velX--
@@ -97,15 +142,3 @@ document.onkeydown = e => {
 document.onkeyup = e => {
     keys[e.key] = false
 }
-// document.onkeydown = e => {
-//     switch (e.key) {
-//         case 'ArrowLeft':
-//             alien.moveLeft()
-//             break;
-//         case 'ArrowRight':
-//             alien.moveRight()
-//             break;
-//         default:
-//             break;
-//     }
-// }
